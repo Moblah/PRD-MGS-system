a8dfb81400a6770c25cd252ff328efac34c154b3
Moblah <dev.kiplagatmeshack@gmail.com>
2025-12-20 23:47:55 +0300

    Final fix: psycopg2-binary + Python 3.12

--- config.py
+++ config.py
@@ -2,7 +2,10 @@ import os
 
 class Config:
     if os.environ.get('DATABASE_URL'):
-        SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+        db_url = os.environ['DATABASE_URL']
+        if db_url.startswith('postgres://'):
+            db_url = db_url.replace('postgres://', 'postgresql://')
+        SQLALCHEMY_DATABASE_URI = db_url
     else:
         SQLALCHEMY_DATABASE_URI = 'sqlite:///instance/app.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
